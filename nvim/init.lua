6d3a04a69982035346a1d442034b6bf4da7c80ff
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2022-09-07 12:09:37 +0900

    Fixed files names

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 -- IMPORTS
-require('plugins')
 -- require('vars')      -- Variables
 require('core/opts')      -- Options
 require('core/colors')      -- Options
+require('plugins')
 -- require('keys')      -- Keymaps
 -- require('plug')      -- Plugins
