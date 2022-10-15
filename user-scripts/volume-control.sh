af05b57ff8a9421c54463e95a374db79e9e60ebc
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2022-10-15 21:31:28 +0900

    switched to pipewire

--- user-scripts/volume-control.sh
+++ user-scripts/volume-control.sh
@@ -4,26 +4,26 @@
 
 ctl=""
 
-sleep_dur=0.03
+sleep_dur=0.02
 
 if [ "$1" = "up" ]
 then
-	ctl="--increase 1"
+	ctl="set-volume --limit 1.0 @DEFAULT_AUDIO_SINK@ 1%+"
 elif [ "$1" = "down" ]
 then
-	ctl="--decrease 1"
+	ctl="set-volume --limit 1.0 @DEFAULT_AUDIO_SINK@ 1%-"
 else
 	echo "Print help message"
 	exit 1
 fi
 
-pamixer --unmute
-pamixer $ctl
+wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
+wpctl $ctl
 sleep $sleep_dur
-pamixer $ctl
+wpctl $ctl
 sleep $sleep_dur
-pamixer $ctl
+wpctl $ctl
 sleep $sleep_dur
-pamixer $ctl
+wpctl $ctl
 sleep $sleep_dur
-pamixer $ctl
+wpctl $ctl
