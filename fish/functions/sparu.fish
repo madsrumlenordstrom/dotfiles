45ef65ba6d23f9c2ec0149c944ec7621e7e21f5a
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-03-31 15:06:02 +0200

    deleted non-functioning function

--- fish/functions/sparu.fish
+++
@@ -1,6 +0,0 @@
-function sparu --description 'Searches Arch and AUR repositories for packages and installs selected packages'
-    set -l packages $(paru -Ssq | fzf -m --preview="paru -Si {}" --preview-window=:hidden --bind=space:toggle-preview)
-    if [ -n "$packages" ]
-        paru -Syy $packages
-    end
-end
