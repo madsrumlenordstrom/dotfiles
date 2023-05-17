f6c6893c9c5ece661e5ebce0d47bdb770c7f5ccb
Mads Rumle Nordstroem <madsrumlenordstrom@icloud.com>
2023-05-17 11:57:23 +0200

    added realpath abbr

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,7 @@ if status is-interactive
     abbr --add syu paru -Syu
     abbr --add yeet paru -Rcns
     abbr --add hx helix
-    abbr --add --set-cursor rp "realpath -z % | wl-copy"
+    abbr --add --set-cursor rp "realpath -z % |xargs --null -I -- echo 'wl-copy --' |sh"
 
     # Git abbreviations
     abbr --add mkdir mkdir -p
@@ -25,7 +25,7 @@ if status is-interactive
     abbr --add gco git checkout
     abbr --add gl git log
     abbr --add gpl git pull
-    abbr --add gcl "wl-paste | xargs -I % echo "git clone %" | sh"
+    abbr --add gcl "wl-paste | xargs -I % echo 'git clone %' | sh"
 
     # Stuff for fzf
     set -x FZF_DEFAULT_COMMAND 'fd --type file --type directory --follow'
